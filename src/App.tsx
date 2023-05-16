8b5b5b6cc89abb16e5ad637fb22b9e8671a740f3
Jenni Tran <jenni90232@gmail.com>
2023-05-16 18:44:45 +0300

    fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,13 @@ import {
   setupIonicReact,
 } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import { Redirect, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { initializeApp } from "firebase/app";
 
 import Menu from "./components/Menu";
 import Page from "./pages/Page";
 import Splash from "./pages/Splash";
 import Contact from "./pages/Contact";
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -69,7 +68,6 @@ const App: React.FC = () => {
             <Menu />
             <IonRouterOutlet id="main">
               <Route path="/" exact={true}>
-              <Redirect to="/home" />
                 <Splash />
               </Route>
               <Route path="/page/:name" exact={true}>
